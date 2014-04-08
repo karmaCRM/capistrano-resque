3bb330fc6df01967884b3a48eec356de6e391cae
shiller <shiller@karmadeploy.(none)>
2014-04-07 22:02:49 -0400

    killing on pidfiles which should exist

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -43,8 +43,8 @@ module CapistranoResque
 
         def stop_command
           "if [ -e #{current_path}/tmp/pids/resque_work_1.pid ]; then \
-           for f in `"ls #{current_path}/tmp/pids/resque_work*.pid"`; \
-             do pid=`cat $f`; #{try_sudo} kill -s #{resque_kill_signal} $pid \
+           for f in 1 2 3; \
+             do pid=`cat #{current_path}/tmp/pids/resque_work_$f`; #{try_sudo} kill -s #{resque_kill_signal} $pid \
              && rm $f ;done \
            ;fi"
         end
